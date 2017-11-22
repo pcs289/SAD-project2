5197661cedc9090b3ca69bc196bd9eafabf73588
Gumersildo <alberto.miras.gil@gmail.com>
2017-11-22 17:34:52 +0100

    fucking nickname not running

--- Server.java
+++ Server.java
@@ -1,6 +1,7 @@
 import java.lang.*;
 import java.io.*;
 import java.net.*;
+import java.util.*;
 import java.util.concurrent.*;
 
 public class Server{
@@ -13,17 +14,38 @@ public class Server{
             MyServerSocket mss = new MyServerSocket(port);
             while (true) {
                 MySocket ms = mss.accept();
-                PrintWriter outms = new PrintWriter(ms.getOutputStream(), true);
-                BufferedReader inms = new BufferedReader(new InputStreamReader(ms.getInputStream()));
-                outms.println("Quin nickname vols tenir?");
-                String nick = inms.readLine();
-                llista.put(nick,ms);
-                System.out.println("Client with nickname: "+ nick +" connection started");
-                new Thread(new Service(ms, nick)).start();
+                String actualNick = this.requestName(ms); 
+                this.llista.put(actualNick, ms);             
+                System.out.println("Client with nickname: "+ actualNick +" connection started");
+                new Thread(new Service(ms, actualNick, this)).start();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public String requestName(MySocket s){
+        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        Boolean check = true;
+        String acceptedNick = null;
+        Set<String> keys = this.llista.keySet();
+        
+        do{
+            out.println("Quin nickname vols tenir?");
+            try{
+                acceptedNick = in.readLine();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            for(String name : keys){
+                if(name.equals(acceptedNick)){
+                    out.println("Aquest nick ja esta en us, tria'n un altre!");
+                    check = false;
+                }else{check = true;}
+            }
+        }while(!check);     
+        return acceptedNick;
     }
     
 }
--- Service.java
+++ Service.java
@@ -1,16 +1,20 @@
 import java.lang.*;
 import java.io.*;
+import java.util.*;
 import java.net.*;
 
 public class Service implements Runnable{
 	MySocket ms;
-    String nick;
+    String currentNick;
+    Server serv;
     public static final String RESET = "\u001B[0m";
     public static final String RED = "\u001B[31m";
+    public static final String YELLOW = "\u001B[33m";
 
-    public Service(MySocket s, String nick) {
+    public Service(MySocket s, String nick, Server serv) {
         this.ms = s;
-        this.nick = nick;
+        this.currentNick = nick;
+        this.serv = serv;
     }
 
     @Override
@@ -24,8 +28,17 @@ public class Service implements Runnable{
                 if (line == null) {
                     break;
                 }
-                // out.println("\\e[1;31m"+nick + ">> \\e[0m" + line);
-                out.println(RED + nick + "-> " + RESET + line);
+
+                Set<String> keySet = this.serv.llista.keySet();
+                System.out.println(keySet.toString());
+                for (String nick : keySet){
+                    if(!nick.equals(currentNick)){
+                        PrintWriter outbroadcast = new PrintWriter(this.serv.llista.get(nick).getOutputStream(), true);
+                        outbroadcast.println(RED + nick + "-> " + RESET + line);
+                    }else {
+                        out.println(YELLOW + currentNick + "-> " + RESET + line);
+                    }
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
